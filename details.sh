12cb27ad3d12081ead272fad09bd184f883ed23c
testuser1 <testuser1@ip-10-1-1-98.ec2.internal>
2020-06-19 01:40:03 +0000

    added sudo code users&list

--- details.sh
+++ details.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 echo "THIS SCRIPT WILL PROVIDE INFORMATION ABOUT THE LOCAL SYSYTEM"
+
+#GIVES HOSTNAME
 echo $(hostname)
+
+#GIVES USER EXECUTED THIS SCRIPT
 echo $(whoami)
-echo $(id -un)
+
+#GIVE USERS IN THE SYSTEM
+USERS=$(cat /etc/passwd | cut -d ":" -f 1)
+echo $USERS
+
+
