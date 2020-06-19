6819b80bb7f1156c646004f9afd0cb737ec7db66
pillaribhanu <testuser1@ip-10-1-1-98.ec2.internal>
2020-06-19 02:12:32 +0000

    Added the ip adress script

--- details.sh
+++ details.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 echo "THIS SCRIPT WILL PROVIDE INFORMATION ABOUT THE LOCAL SYSYTEM"
-
+echo
 #GIVES HOSTNAME
 echo $(hostname)
-
+echo
 #GIVES USER EXECUTED THIS SCRIPT
 echo $(whoami)
-
+echo
 #GIVE USERS IN THE SYSTEM
 USERS=$(cat /etc/passwd | cut -d ":" -f 1)
 echo $USERS
-
-
+echo
+#GET IP ADRESS OF THE SYSTEM
+IP=$(ifconfig)
+echo $IP
